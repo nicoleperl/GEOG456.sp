078106f11a8fb71b7bfbc39fc9f458594b9471ab
Nicole Perl <nicole.perl@icloud.com>
2026-01-22 18:03:20 -0500

    HW2 Upload

---
+++ HW2.js
@@ -0,0 +1,7 @@
+var cath = {
+"type": "FeatureCollection",
+"crs": { "type": "name", "properties": { "name": "urn:ogc:def:crs:OGC:1.3:CRS84" } },
+"features": [
+{ "type": "Feature", "properties": { "id": 1, "descrip": "museum" }, "geometry": { "type": "Point", "coordinates": [ -8.544449047524168, 42.88092382451022] } }
+]
+}
